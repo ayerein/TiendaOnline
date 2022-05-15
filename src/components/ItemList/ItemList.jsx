4a551526ccce1e6ed542a582057114d65e63016f
Ayelen Rein <ayelenreinglestein@gmail.com>
2022-05-14 22:15:24 -0300

    Desafio clase 5, corrección

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,36 +1,9 @@
-import { useEffect, useState } from "react"
-import {productos} from '../../data/data'
 import Item from "../Item/Item"
 import './ItemList.css'
 
 
-const ItemList = () => {
-    const [products, setProducts] = useState([])
-
-    const getFetch = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve( productos )
-        }, 2000)
-    })
-
-    const getProductsFromDB = async () => {
-        try{
-            const result = await getFetch;
-            setProducts(result);
-        } catch (error){
-            console.log(error)
-        }
-    }
-
-
-    useEffect(() => {
-        getProductsFromDB()
-    }, )
-
+const ItemList = ({products}) => {
     return (
-        <div className="contenedor-itemlist">
-          {
-            products.length ? (
               <div className="contenedor-productos">
                 {   
                     products.map((product) => {
@@ -43,16 +16,11 @@ const ItemList = () => {
                           precio={product.precio}
                           id={product.id}
                           />
-                      </div>
-                    );
-                })
+                            </div>
+                        );
+                    })
                 }
               </div>
-            ) : (
-              <img className="img-loading" src="https://icon-library.com/images/loading-icon-gif/loading-icon-gif-10.jpg" alt="cargando" />
-            )
-          }
-        </div>
       );
         
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,42 @@
 import ItemList from '../ItemList/ItemList';
+import { useEffect, useState } from "react"
+import {productos} from '../../data/data'
 
 const ItemListContainer = () => {
+    const [products, setProducts] = useState([])
+
+    const getProductsFromDB = async () => {
+        try{
+            const result = await getFetch;
+            setProducts(result);
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getProductsFromDB()
+    }, )
+
+    const getFetch = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve( productos )
+        }, 2000)
+    })
+
+
     
     return(
-    <ItemList />
+        <div className="contenedor-itemlist">
+          {
+            products.length ? (
+        <ItemList products={products}/>
+        ) : (
+            <img className="img-loading" src="https://icon-library.com/images/loading-icon-gif/loading-icon-gif-10.jpg" alt="cargando" />
+          )
+        }
+      </div>
+                  
     )
 }
 
